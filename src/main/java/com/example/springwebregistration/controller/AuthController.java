93a61ef0116af5f666512f2efd86c9d8809e4420
Masoomeh Aslani <masiw.rad7@gmail.com>
2024-08-31 01:34:59 +0330

    completed but without validation yet :(

--- src/main/java/com/example/springwebregistration/controller/AuthController.java
+++ src/main/java/com/example/springwebregistration/controller/AuthController.java
@@ -1,2 +1,35 @@
-package com.example.springwebregistration.controller;public class AuthController {
+package com.example.springwebregistration.controller;
+
+
+import com.example.springwebregistration.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+    UserDto user;
+
+    @GetMapping("/register")
+    public String home(Model model){
+        user = new UserDto();
+        model.addAttribute("user", user);
+       return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registerSave(@ModelAttribute("user") UserDto user){
+        this.user.setFirstName(user.getFirstName());
+        this.user.setLastName(user.getLastName());
+        this.user.setEmail(user.getEmail());
+        return "redirect:/result";
+    }
+
+    @GetMapping("/result")
+    public String result(Model model) {
+        model.addAttribute("user", user);
+        return "result";
+    }
 }
--- src/main/java/com/example/springwebregistration/dto/UserDto.java
+++ src/main/java/com/example/springwebregistration/dto/UserDto.java
@@ -1,2 +1,41 @@
-package com.example.springwebregistration.dto;public class UserDto {
+package com.example.springwebregistration.dto;
+
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
